e999d381eb9e2bd9fd5125a8ea1a886487f83232
kebincontreras <kebinandrescontreras@gmail.com>
2024-03-15 12:28:15 -0500

    Best

--- Visualization.py
+++ Visualization.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from load_data import cargar_datos 
+from load_data import cargar_datos, graficar_firmas_espectrales
 import matplotlib.pyplot as plt
 
 
@@ -7,33 +7,10 @@ import matplotlib.pyplot as plt
 ruta_base = "C:/Users/USUARIO/Documents/dataset_cocoa_hdsp"
 nombre_blanco = "BLANCO_1.csv"
 nombre_lote = "LOTE3EJ_1.csv"
+NO_firmas = 200
 
 lote, blanco_saturado, blanco_ref, wavelengths = cargar_datos(ruta_base, nombre_blanco, nombre_lote)
-
-# Cálculo del factor de escala y ajuste
-factor_de_escala = lote.iloc[1, 1608] / blanco_ref.iloc[1608]  
-blanco_escalado = blanco_ref * factor_de_escala
-
-
-# Cálculo del factor de escala y ajuste
-factor_de_escala = lote.iloc[1, 1608] / blanco_ref.iloc[1608]  
-blanco_escalado = blanco_ref * factor_de_escala
-
-# Visualización de blanco escalado vs. blanco saturado
-plt.figure()
-plt.plot(wavelengths, blanco_escalado, label='Blanco escalado')
-plt.plot(wavelengths, lote.iloc[1, :], label='Blanco saturado')  # Asumiendo que quieres comparar estos dos
-plt.legend()
-plt.show()
-
-# Para visualizar las firmas espectrales
-plt.figure()
-plt.plot(wavelengths, blanco_escalado, label='Blanco escalado')
-# Usar slicing para seleccionar rangos específicos si es necesario
-plt.plot(wavelengths, lote.iloc[3::100, :], label='LOTE3EJ1')
-plt.legend()
-plt.show()
-
+graficar_firmas_espectrales(blanco_ref, lote, wavelengths, blanco_saturado, NO_firmas)
 
 
 
--- load_data.py
+++ load_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def cargar_datos(ruta_base, nombre_blanco, nombre_lote):
 
@@ -16,3 +17,33 @@ def cargar_datos(ruta_base, nombre_blanco, nombre_lote):
     blanco_saturado = lote.iloc[1, :]
 
     return lote, blanco_saturado, blanco_ref, wavelengths
+
+
+def graficar_firmas_espectrales(blanco_ref, lote, wavelengths, blanco_saturado, num_firmas):
+   
+    # Cálculo del factor de escala y ajuste
+    factor_de_escala = lote.iloc[1, 1608] / blanco_ref.iloc[1608]
+    blanco_escalado = blanco_ref * factor_de_escala
+
+    # Visualización de blanco escalado vs. blanco saturado
+    plt.figure()
+    plt.plot(wavelengths, blanco_escalado, label='Blanco escalado')
+    plt.plot(wavelengths, blanco_saturado, label='Blanco saturado')
+    plt.legend()
+    plt.show(block=False)
+
+    # Visualización de firmas espectrales seleccionadas del lote contra el blanco escalado
+    plt.figure()
+    plt.plot(wavelengths, blanco_escalado, label='Blanco escalado', linewidth=2, color='k')
+
+    # Asegurar que no se intenta graficar más firmas de las disponibles
+    num_firmas = min(num_firmas, lote.shape[0] - 3)
+
+    for i in range(3, 3 + num_firmas):
+        plt.plot(wavelengths, lote.iloc[i, :], label=f'Firma LOTE - Muestra {i}')
+
+    # Omitir la leyenda si hay muchas líneas para mantener la claridad
+    # if num_firmas <= 10:
+    #     plt.legend()
+
+    plt.show()
